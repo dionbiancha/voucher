ba2179d28683525cfbd58ee5a9057e3263d4938a
Dionei Bianchati <dion.biancha@gmail.com>
2024-02-20 07:53:14 -0300

    feat: init i18n

---
+++ src/context/userContext.tsx
@@ -0,0 +1,38 @@
+import { User } from 'firebase/auth'
+import React, { createContext, useState, ReactNode, useContext } from 'react'
+
+interface UserContextProps {
+  userData: User
+  setUserData: React.Dispatch<React.SetStateAction<User>>
+}
+
+export const UserContext = createContext<UserContextProps | undefined>(undefined)
+
+interface UserProviderProps {
+  children: ReactNode
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [userData, setUserData] = useState<User>({} as User)
+
+  return (
+    <UserContext.Provider
+      value={{
+        setUserData,
+        userData
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const usePreview = () => {
+  const context = useContext(UserContext)
+
+  if (!context) {
+    throw new Error('usePreview must be used within a PreviewProvider')
+  }
+
+  return context
+}
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,52 @@
-import { Box, Button, Container, Flex, Heading, Link, Text } from '@radix-ui/themes'
-import { FaGoogle } from 'react-icons/fa'
-import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  TextField
+} from '@radix-ui/themes'
+import { FaGoogle, FaFacebookF } from 'react-icons/fa'
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  User,
+  FacebookAuthProvider,
+  createUserWithEmailAndPassword
+} from 'firebase/auth'
 import { auth } from '../../services/firebase'
 import { useState } from 'react'
 
 function SignIn() {
   const [user, setUser] = useState<User>({} as User)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
   function handleGoogleSignIn() {
     const provider = new GoogleAuthProvider()
     signInWithPopup(auth, provider)
       .then((res) => {
         setUser(res.user)
+      })
+      .catch((error) => {})
+  }
+
+  function handleFacebookSignIn() {
+    const provider = new FacebookAuthProvider()
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        console.log(res.user)
+      })
+      .catch((error) => {})
+  }
+
+  function handleEmailAndPasswordSignIn() {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log(res.user)
       })
       .catch((error) => {})
   }
@@ -23,9 +58,10 @@ function SignIn() {
           Welcome to Voucher
         </Heading>
         <Text>Crie vouchers de maneira rapida e simples</Text>
+
         <Button
           onClick={handleGoogleSignIn}
-          style={{ width: '300px' }}
+          style={{ width: '300px', cursor: 'pointer' }}
           mt='5'
           color='tomato'
           radius='large'
@@ -33,6 +69,41 @@ function SignIn() {
         >
           <FaGoogle width='16' height='16' /> Google
         </Button>
+
+        <Button
+          onClick={handleFacebookSignIn}
+          style={{ width: '300px', cursor: 'pointer' }}
+          mt='2'
+          color='blue'
+          radius='large'
+          size='2'
+        >
+          <FaFacebookF width='16' height='16' /> Facebook
+        </Button>
+        <Text mt='3'>or</Text>
+        <Flex direction='column' gap='3' mt='3' style={{ width: 300 }}>
+          <TextField.Input
+            onChange={(e) => setEmail(e.target.value)}
+            variant='surface'
+            placeholder='Enter your email'
+          />
+          <TextField.Input
+            onChange={(e) => setPassword(e.target.value)}
+            variant='surface'
+            placeholder='Enter your password'
+            type='password'
+          />
+          <Button
+            onClick={handleEmailAndPasswordSignIn}
+            style={{ width: '300px', cursor: 'pointer', backgroundColor: '#000' }}
+            mt='5'
+            radius='large'
+            size='3'
+          >
+            Create Account
+          </Button>
+        </Flex>
+
         <Text mt='5' size='1'>
           By registration you agree to <Link>Terms of Use</Link> and{' '}
           <Link>Privacy Policy</Link>
